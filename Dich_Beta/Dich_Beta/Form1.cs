6576ea623ef67a068ca7f36d62c7566b0194ea19
Ivan <email>
2019-11-25 15:11:12 +0200

    Hi

--- Dich_Beta/Dich_Beta/Form1.cs
+++ Dich_Beta/Dich_Beta/Form1.cs
@@ -26,5 +26,10 @@ namespace Dich_Beta
         {
             label1.Text = "Yes";
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi");
+        }
     }
 }
